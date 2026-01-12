0e0f0d1863374cf6589b80870cb7629e28cb9218
Kathe <katta.vergara.libertad@gmail.com>
2026-01-12 13:51:33 -0300

    Agregue validacion de coche nuevo o usado para imprimir en consola

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ marca = input("Ingrese la marca del coche:")
 modelo = input("Ingrese el modelo del coche:")
 anio = int(input("Ingrese el año del coche:"))
 color = input("Ingrese el color del coche:")
+
+#creamos el objeto usando la clase
+coche = Coche(marca, modelo, anio, color)
+
+#verificamos si el coche es nuevo o usado
+if coche.anio >= 2022:
+    print("El coche es NUEVO")
+else:
+    print("El coche es USADO")
+    
+
